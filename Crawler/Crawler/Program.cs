80192737923195e965586ec20202e8e3132a3233
Mroxny <50219282+Mroxny@users.noreply.github.com>
2023-03-08 13:18:23 +0100

    Init poject

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Crawler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
