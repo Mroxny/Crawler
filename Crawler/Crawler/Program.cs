2b847c4e952140cb9c8049199ec05ca336d5a8c4
Mroxny <50219282+Mroxny@users.noreply.github.com>
2023-03-08 13:47:31 +0100

    Make httpClient

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -4,12 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello, Worl");
-
-            for (int i = 0; i < args.Length; i++) {
-                Console.WriteLine(args[i]);
+            if (args.Length <= 0)
+            {
+                throw new ArgumentNullException();
             }
+
+            if (args.Length > 1)
+            {
+                throw new ArgumentException();
+            }
+
+            string[] emails = getEmails(args[0]);
+
+            foreach (string s in emails) {
+                Console.WriteLine(s);
+            }
+        }
+
+        static async string[] getEmails(string url) {
+            using HttpClient httpClient = new();
+
+            HttpResponseMessage resul = await httpClient.GetAsync(url).Result;
+            
+
+            //httpClient.Dispose();
+            return new string[0];
+
         }
     }
 }
